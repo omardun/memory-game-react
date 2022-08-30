91be308b5bca08ea18b210fda8166f25c4301790
omardun <omar.rosquero10@gmail.com>
2022-08-29 21:04:01 -0400

    Setting up the initial application state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,78 @@
-// import React from 'react';
+import React from 'react';
 import './App.css';
 import MemoryCard from "./components/MemoryCard";
 
 
-function App() {
-  
-  return (
-    <div className="App">
-    
-      <header className="App-header">
-        <>
-          Memory Game
-        </>
-        <a
-          className="App-link"
-        >
-          Match cards to win
-        </a>
-      </header>
-      <div>
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      </div>
-    </div>
-  );
+let generateDeck = () => {
+  let symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø']
+  for (let i = 0; i < 16; i++) {
+
+    let deck = [];
+
+    let card = {
+      isFlipped: false,
+      symbol: symbols[i % 8]
+    }
+    deck.push(card);
+    shuffle(deck)
+    return deck;
+  }
+
+
 }
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state =  {generateDeck()}
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <>
+            Memory Game
+          </>
+          <a
+            className="App-link"
+          >
+            Match cards to win
+          </a>
+        </header>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
 
 export default App;
